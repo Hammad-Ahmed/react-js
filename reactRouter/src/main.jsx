4e5ce41276cab04e71c6d4f61a8207522c470e4f
Hammad Ahmed <hammadahmed.saifee.me@gmail.com>
2025-10-14 17:09:01 +0500

    React router structure

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Layout from './Layout.jsx'
+import { BrowserRouter, createBrowserRouter } from "react-router-dom";
+import Home from './components/Pages/Home.jsx';
+
+// const router = createBrowserRouter([
+//   { 
+//     path: "/", 
+//     element: <Layout /> 
+//   },
+// ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
   </StrictMode>,
 )
